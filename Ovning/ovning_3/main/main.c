94c7a5933df0a76ed15796df70def37433a6c701
Rasmus <91119694+Rasweb@users.noreply.github.com>
2025-02-04 15:07:21 +0100

    Små fixar
    * Skrev några rader för att hantera max och min duty
    * Tog bort kommentarer

--- Ovning/ovning_3/main/main.c
+++ Ovning/ovning_3/main/main.c
@@ -65,8 +65,8 @@ void app_main(void) {
                 starTick_2 = xTaskGetTickCount();
                 latch_2 = 1;
                 duty += 50;
+                if(duty > 4095) duty = 4095;
                 ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE,LEDC_CHANNEL_0, duty, 0);
-                
             }
         }
         else if(pull_down == 0 && latch_2 == 1){
@@ -84,6 +84,7 @@ void app_main(void) {
                 starTick_3 = xTaskGetTickCount();
                 latch_3 = 1;
                 duty -= 50;
+                if(duty < 0) duty = 0;
                 ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty, 0);
             }
         }
@@ -97,62 +98,3 @@ void app_main(void) {
 }
 
 
-
-
-//     gpio_pad_select_gpio(GPIO_NUM_2); // Change to your desired GPIO pin
-//     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
-
-
-// #include <stdio.h>
-// #include "driver/ledc.h"
-// #include "esp_err.h"
-
-// #define LEDC_TIMER              LEDC_TIMER_0
-// #define LEDC_MODE               LEDC_LOW_SPEED_MODE
-// #define LEDC_OUTPUT_IO          (0) // Define the output GPIO
-// #define LEDC_CHANNEL            LEDC_CHANNEL_0
-// #define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-// #define LEDC_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
-// #define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
-
-// /* Warning:
-//  * For ESP32, ESP32S2, ESP32S3, ESP32C3, ESP32C2, ESP32C6, ESP32H2, ESP32P4 targets,
-//  * when LEDC_DUTY_RES selects the maximum duty resolution (i.e. value equal to SOC_LEDC_TIMER_BIT_WIDTH),
-//  * 100% duty cycle is not reachable (duty cannot be set to (2 ** SOC_LEDC_TIMER_BIT_WIDTH)).
-//  */
-
-// static void example_ledc_init(void)
-// {
-//     // Prepare and then apply the LEDC PWM timer configuration
-//     ledc_timer_config_t ledc_timer = {
-//         .speed_mode       = LEDC_MODE,
-//         .duty_resolution  = LEDC_DUTY_RES,
-//         .timer_num        = LEDC_TIMER,
-//         .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
-//         .clk_cfg          = LEDC_AUTO_CLK
-//     };
-//     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
-
-//     // Prepare and then apply the LEDC PWM channel configuration
-//     ledc_channel_config_t ledc_channel = {
-//         .speed_mode     = LEDC_MODE,
-//         .channel        = LEDC_CHANNEL,
-//         .timer_sel      = LEDC_TIMER,
-//         .intr_type      = LEDC_INTR_DISABLE,
-//         .gpio_num       = LEDC_OUTPUT_IO,
-//         .duty           = 0, // Set duty to 0%
-//         .hpoint         = 0
-//     };
-//     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
-// }
-
-// void app_main(void)
-// {
-//     gpio_config(LEDC_OUTPUT_IO, )
-//     // Set the LEDC peripheral configuration
-//     example_ledc_init();
-//     // Set duty to 50%
-//     ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
-//     // Update duty to apply the new value
-//     ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
-// }
