ddd7242ccee578ad8e6a27e934c5a838a98e307d
Rasmus <91119694+Rasweb@users.noreply.github.com>
2025-02-07 15:55:32 +0100

    i2c slav start kod

--- Ovning/ovning_5/main/main.c
+++ Ovning/ovning_5/main/main.c
@@ -1,8 +1,61 @@
+#include "driver/i2c_slave.h"
+#include "freertos/FreeRTOS.h"
+
+#define I2C_SLAVE_NUM GPIO_NUM_5
+#define I2C_SLAVE_SCL_IO GPIO_NUM_7
+#define I2C_SLAVE_SDA_IO GPIO_NUM_6
+#define ESP_SLAVE_ADDR 1
+
+i2c_slave_config_t i2c_slv_config = {
+    .i2c_port = I2C_SLAVE_NUM,
+    .clk_source = I2C_CLK_SRC_DEFAULT,
+    .scl_io_num = I2C_SLAVE_SCL_IO,
+    .sda_io_num = I2C_SLAVE_SDA_IO,
+    .slave_addr = ESP_SLAVE_ADDR,
+    .send_buf_depth = 100,
+};
+
+i2c_slave_dev_handle_t slave_handle;
+
+typedef enum
+{
+    I2C_SLAVE_EVT_RX,
+    I2C_SLAVE_EVT_TX
+} i2c_slave_event_t;
+
+QueueHandle_t queue;
+
+// Slave Read
+static bool i2c_slave_receive_cb(i2c_slave_dev_handle_t i2c_slave, const i2c_slave_rx_done_event_data_t *evt_data, void *arg)
+{
+    i2c_slave_event_t evt = I2C_SLAVE_EVT_RX;
+    BaseType_t xTaskWoken = 0;
+    // You can get data and length via i2c_slave_rx_done_event_data_t
+    // xQueueSendFromISR(context->event_queue, &evt, &xTaskWoken);
+    xQueueCRSendFromISR(queue) return xTaskWoken;
+}
+
+// Register callback in a task
+i2c_slave_event_callbacks_t cbs = {
+    .on_receive = i2c_slave_receive_cb,
+};
+ESP_ERROR_CHECK(i2c_slave_register_event_callbacks(context.handle, &cbs, &context));
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(i2c_new_slave_device(&i2c_slv_config, &slave_handle));
+
+    // Uninstall Slave
+    // i2c_del_slave_device(&slave_handle);
+}
+
+/* - WORKING UART
 #include <stdio.h>
 #include "driver/gpio.h"
 #include "driver/uart.h"
 #include <string.h>
 #include "esp_log.h"
+
 
 #define TX_GPIO_PIN GPIO_NUM_5
 #define RX_GPIO_PIN GPIO_NUM_4
@@ -20,27 +73,24 @@ uart_config_t uart_config = {
     .stop_bits = UART_STOP_BITS_1,
     .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
 
-QueueHandle_t uart_queue;
+    QueueHandle_t uart_queue;
+
 
 void app_main(void)
 {
+
     ESP_ERROR_CHECK(uart_driver_install(uart_num, TX_BUFFER_SIZE, RTX_BUFFER_SIZE, 10, &uart_queue, 0));
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
 
-    // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
-    // ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 4, 5, 18, 19));
-
     ESP_ERROR_CHECK(uart_set_pin(uart_num, TX_GPIO_PIN, RX_GPIO_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-
-    // ESP_ERROR_CHECK(uart_driver_install(uart_num, TX_BUFFER_SIZE, RTX_BUFFER_SIZE, 10, &uart_queue, 0));
 
     TickType_t sentTimer = xTaskGetTickCount();
     while (1)
     {
 
-        TickType_t elapsedTime = xTaskGetTickCount() - sentTimer;
-        if (elapsedTime > pdMS_TO_TICKS(SEND_UART_PERIOD))
-        {
+    TickType_t elapsedTime = xTaskGetTickCount() - sentTimer;
+    if (elapsedTime > pdMS_TO_TICKS(SEND_UART_PERIOD))
+    {
             sentTimer += pdMS_TO_TICKS(SEND_UART_PERIOD);
             // Write data to UART.
             char *test_str = "Hej hej William.\n";
@@ -75,8 +125,6 @@ void app_main(void)
         }
         else
         {
-            // ESP_LOGI("MAIN", "Bytes in buffer %d", length);
-
             vTaskDelay(pdMS_TO_TICKS(10));
             continue;
         }
@@ -95,18 +143,5 @@ void app_main(void)
             ESP_LOGI("MAIN", "Received no bytes on uart");
         }
     }
-
-    //  // Configure a temporary buffer for the incoming data
-    //  uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-
-    //  while (1) {
-    //      // Read data from the UART
-    //      int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
-    //      // Write data back to the UART
-    //      uart_write_bytes(ECHO_UART_PORT_NUM, (const char *) data, len);
-    //      if (len) {
-    //          data[len] = '\0';
-    //          ESP_LOGI(TAG, "Recv str: %s", (char *) data);
-    //      }
-    //  }
+    */
 }
